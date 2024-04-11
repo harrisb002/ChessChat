0d8ea88793617c763fb3869f379c7508ef358291
harrisb002 <harrisbe@sonoma.edu>
2024-04-11 02:49:08 -0700

    file uploading

--- chess-chat/components/file-upload.tsx
+++ chess-chat/components/file-upload.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
@@ -7,7 +11,17 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  return <div>File Upload Component</div>;
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
 };
 
 export default FileUpload;
