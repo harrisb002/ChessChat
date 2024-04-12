5f76c512739e1b6bc43210de6de5e123949dd372
harrisb002 <harrisbe@sonoma.edu>
2024-04-12 05:06:08 -0700

    Create link route

---
+++ chess-chat/app/api/clubs/[clubId]/invite-code/route.ts
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from "uuid";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      clubId: string;
+    };
+  }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.clubId) {
+      return new NextResponse("Club Id Missing", { status: 400 });
+    }
+
+    // Update the club
+    const club = await db.club.update({
+      where: {
+        id: params.clubId,
+        profileId: profile.id,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+
+    return NextResponse.json(club);
+    
+  } catch (error) {
+    console.log("[CLUB_ID", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
