43f329fdac5497bc325d387260622be7b502122c
harrisb002 <harrisbe@sonoma.edu>
2024-04-12 04:52:39 -0700

    Generating invite links

--- chess-chat/components/modals/invite-modal.tsx
+++ chess-chat/components/modals/invite-modal.tsx
@@ -16,12 +16,13 @@ import {
 } from "@/components/ui/dialog";
 import { UseOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
+import axios from "axios";
 
 export const InviteModal = () => {
   const [copied, setIsCopied] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { isOpen, onClose, type, data } = useModal();
+  const { onOpen, isOpen, onClose, type, data } = useModal();
   const origin = UseOrigin();
 
   const isModalOpen = isOpen && type === "invite";
@@ -41,6 +42,20 @@ export const InviteModal = () => {
     }, 1500);
   };
 
+  const onNewLink = async () => {
+    try {
+      setIsLoading(true);
+      // Refresh the invite code
+      const response = await axios.patch(`/api/clubs/${club?.id}/invite-code`);
+      // Refresh with the new data
+      onOpen("invite", { club: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -55,10 +70,11 @@ export const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-bkack focus-visible:ring-offset-0"
               value={inviteUrl}
             />
-            <Button onClick={onCopy} size="icon">
+            <Button disabled={isLoading} onClick={onCopy} size="icon">
               {copied ? (
                 <Check className="w-4 h-4" />
               ) : (
@@ -67,6 +83,8 @@ export const InviteModal = () => {
             </Button>
           </div>
           <Button
+            onClick={onNewLink}
+            disabled={isLoading}
             variant="link"
             size="sm"
             className="text-xs text-zinc-500 mt-4"
