b86da25435f946bf60a47d86ab70b796307bb008
harrisb002 <harrisbe@sonoma.edu>
2024-04-11 17:11:34 -0700

    Club rendering in Navbar

--- chess-chat/components/navigation/naviagtion-item.tsx
+++ chess-chat/components/navigation/naviagtion-item.tsx
@@ -14,5 +14,28 @@ interface NavigationItemProps {
 }
 
 export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
-  return <div>Club</div>;
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <button onClick={() => {}} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.clubId !== id && "group-hover:h-[20px]",
+            params?.clubId === id ? "h-[36px]" : "h-8px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.clubId === id && "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt="Channel" />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
 };
