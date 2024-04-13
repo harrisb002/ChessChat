deab2f67cdf17536c0a39e62b995fae2e3566419
harrisb002 <harrisbe@sonoma.edu>
2024-04-13 03:45:19 -0700

    creating new channel route

--- chess-chat/components/modals/create-channel-modal.tsx
+++ chess-chat/components/modals/create-channel-modal.tsx
@@ -1,10 +1,13 @@
 "use client";
 import * as z from "zod";
 import axios from "axios"
+import qs from "query-string";
+import { useForm } from "react-hook-form";
+import { useParams, useRouter } from "next/navigation";
+
+import { ChannelType } from "@prisma/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useModal } from "@/hooks/use-modal-store";
-import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -26,22 +29,39 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
+
 const formSchema = z.object({
   name: z.string().min(1, {
-    message: "Club name is required.",
-  })
+    message: "Channel name is required.",
+  }).refine(  // Cannot use general as the Channel name
+    name => name !== "general",
+    {
+      message: "Channel name cannot be 'general'"
+    }
+  ),
+  type: z.nativeEnum(ChannelType)
 });
 
 export const CreateChannelModal = () => {
   const { isOpen, onClose, type } = useModal();
   const router = useRouter();
+  const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
 
+  // Resolve form submition using formschema
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
+      type: ChannelType.TEXT,
     },
   });
 
@@ -50,7 +70,15 @@ export const CreateChannelModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/clubs", values);
+      const url = qs.stringifyUrl({
+        url: "/api/channels",
+        query: {
+          clubId: params?.clubId
+        }
+      })
+      // Use the queried url with data
+      await axios.post(url, values);
+
       form.reset();
       router.refresh();
       onClose();
@@ -81,26 +109,60 @@ export const CreateChannelModal = () => {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                      Club Name
+                      Channel Name
                     </FormLabel>
                     <FormControl>
                       <Input
                         disabled={isLoading}
                         className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter Club Name"
+                        placeholder="Enter Channel Name"
                         {...field}
-                      ></Input>
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <DialogFooter className="bg-gray-100 px-6 py-4">
-                <Button variant="primary" disabled={isLoading}>
-                  Create
-                </Button>
-              </DialogFooter>
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Channel Type</FormLabel>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger
+                          className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
+                        >
+                          <SelectValue placeholder="Select a channel type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Object.values(ChannelType).map((type) => (
+                          <SelectItem
+                            key={type}
+                            value={type}
+                            className="capitalize"
+                          >
+                            {type.toLowerCase()}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
           </form>
         </Form>
 
