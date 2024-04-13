1dc3370ec76e8f45c5390f3f9afa06ac72b1ef5a
harrisb002 <harrisbe@sonoma.edu>
2024-04-13 01:07:12 -0700

    Creating dropdown for managing members

--- chess-chat/components/modals/members-modal.tsx
+++ chess-chat/components/modals/members-modal.tsx
@@ -3,6 +3,17 @@ import { ClubWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useModal } from "@/hooks/use-modal-store";
 import { UserAvatar } from "@/components/user-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger
+} from "@/components/ui/dropdown-menu"
 
 import {
   Dialog,
@@ -11,16 +22,18 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { ShieldCheck } from "lucide-react";
+import { Check, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
+import { useState } from "react";
 
 const roleIconMap = {
   "GUEST": null,
-  "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  "MODERATOR": <ShieldAlert className="h-4 w-4 ml-2 text-indigo-500" />,
   "ADMIN": <ShieldCheck className="h-4 w-4 text-green-600" />
 }
 
 export const MembersModal = () => {
   const { isOpen, onClose, type, data } = useModal();
+  const [loadingId, setLoadingId] = useState((""));
 
   const isModalOpen = isOpen && type === "members";
 
@@ -47,7 +60,41 @@ export const MembersModal = () => {
                   {member.profile.name}
                   {roleIconMap[member.role]}
                 </div>
+                <p className="text-xs text-zinc-500">
+                  {member.profile.email}
+                </p>
               </div>
+              {/* club.profileId !== member.profile.id && */}
+              {loadingId !== member.id && (
+                <div className="ml-auto">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className="h-4 w-4 text-zinc-500" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="left">
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger className="flex items-center">
+                          <ShieldQuestion className="w-4 h-4 mr-2" />
+                          <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem>
+                              <Shield className="h-4 w-4 mr-2" />
+                              Guest
+                              {member.role === "GUEST" && (
+                                <Check
+                                  className="h-4 w-4 ml-auto"
+                                />
+                              )}
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              )}
             </div>
           ))}
         </ScrollArea>
