cc3fa129437b8570bf6d5c770e0f6e7834f7fc3c
harrisb002 <harrisbe@sonoma.edu>
2024-04-19 20:41:52 -0700

    Working on Searching

--- chess-chat/components/club/club-search.tsx
+++ chess-chat/components/club/club-search.tsx
@@ -1,23 +1,60 @@
-"use client"
+"use client";
+
+import { Search } from "lucide-react";
+import { useState } from "react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
 
 interface ClubSearchProps {
-    data: {
-        label: string;
-        type: "channel" | "member",
-        data: {
-            icon: React.ReactNode;
-            name: string,
-            id: string;
-        }[] | undefined
-    }[]
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
 }
 
-export const ClubSearch = ({
-    data
-}: ClubSearchProps) => {
-    return (
-        <div>
-            Club Search Component
-        </div>
-    )
-}
+export const ClubSearch = ({ data }: ClubSearchProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all Channels and Members" />
+        <CommandList>
+          <CommandEmpty>No Results Found</CommandEmpty>
+          {data.map(({label, type, data}) => {
+            if(!data?.length) return null;
+
+            return (
+              <CommandGroup key={label} heading={label}>
+                
+              </CommandGroup>
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
