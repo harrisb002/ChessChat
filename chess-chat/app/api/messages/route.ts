2a0e0f7a334eb21dee1ebb1c90f3c7b40fb6e9eb
harrisb002 <harrisbe@sonoma.edu>
2024-04-26 01:50:34 -0700

    Using Cursor for pagination

---
+++ chess-chat/app/api/messages/route.ts
@@ -0,0 +1,92 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { Message } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+// Fetching 20 X 20 messages for batches
+const MESSAGES_BATCH = 20;
+
+export async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    // Cursor will be used to tell the infinite load from what message to load the next batch of messages
+    // this pagination can be done in other ways, like using the skip method.
+    const cursor = searchParams.get("cursor");
+    const channelId = searchParams.get("channelId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!channelId) {
+      return new NextResponse("Channel Id Missing", { status: 400 });
+    }
+
+    let messages: Message[] = [];
+
+    // Query from the curson if defined
+    if (cursor) {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1, // Dont want to repeat the message which the cursor is on
+        cursor: {
+          // Built in from prisma for pagination
+          id: cursor,
+        },
+        where: {
+          channelId, // paramKey being used in use-chat-query useChatQuery()
+        },
+        include: {
+          // Get all members and their profiles to render all messages
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc", // Reverse messages
+        },
+      });
+    }
+
+    // If we dont have a cursor
+    else {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
+
+    let nextCusor = null;
+
+    // Create the next cursor for the infinte load using the length of the messages
+    // If messages.length < MESSAGES_BATCH then it has hit end for infinite load, ie no more messages
+    if (messages.length === MESSAGES_BATCH) {
+      nextCusor = messages[MESSAGES_BATCH - 1].id;
+    }
+
+    // Give the data back
+    return NextResponse.json({
+      items: messages,
+      nextCusor,
+    });
+  } catch (error) {
+    console.log("[MESSAGES_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- chess-chat/components/chat/chat-messages.tsx
+++ chess-chat/components/chat/chat-messages.tsx
@@ -3,7 +3,7 @@
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
-import { Loader2 } from "lucide-react";
+import { Loader2, ServerCrash } from "lucide-react";
 
 // USed as modular props to be used both for one-one convos as well as channel convos
 interface ChatMessagesProps {
@@ -43,11 +43,18 @@ export const ChatMessages = ({
 
   if (status === "pending") {
     return (
-      <div className="felx flex-col flex-1 justify-center items-center">
+      <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">
-            ...Loading
-        </p>
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">...Loading</p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-5 w-5 text-rose-300 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">...Something went wrong</p>
       </div>
     );
   }
