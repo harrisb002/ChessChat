33f7bfcf7bff7b987f5b17a17bac5458a4f2dfe7
harrisb002 <harrisbe@sonoma.edu>
2024-04-26 01:08:19 -0700

    Creating chat query

--- chess-chat/components/chat/chat-messages.tsx
+++ chess-chat/components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2 } from "lucide-react";
 
 // USed as modular props to be used both for one-one convos as well as channel convos
 interface ChatMessagesProps {
@@ -29,6 +31,27 @@ export const ChatMessages = ({
   paramKey,
   paramValue,
 }: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    });
+
+  if (status === "pending") {
+    return (
+      <div className="felx flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+            ...Loading
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
---
+++ chess-chat/hooks/use-chat-query.tsx
@@ -0,0 +1,59 @@
+import qs from "query-string";
+import { QueryFunctionContext, useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/socket-provider";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+// This can be used for both one-on-one conversations as well as direct messages by using
+// the paramKey & paramValue ("channelId" | "conversation")
+export const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+
+  // Use QueryFunctionContext to use custom queryFn to call REFS: https://tanstack.com/query/latest/docs/framework/react/guides/query-functions#queryfunctioncontext
+  const fetchMessages = async (context: QueryFunctionContext) => {
+    // pageParam will act as the cursor to allow for infinite loading
+    const { pageParam } = context; // Extracting pageParam from the context
+
+    const url = qs.stringify(
+      {
+        api: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  // For Reference: https://tanstack.com/query/latest/docs/framework/react/reference/useInfiniteQuery#useinfinitequery
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey],
+      queryFn: fetchMessages, // Function to call to request data
+      initialPageParam: 1,
+      getNextPageParam: (lastPage) => lastPage?.nextCursor, // Incase the API fails then use to reshresh
+      refetchInterval: isConnected ? false : 1000, // Can rely on this if websockets not available
+    });
+
+  return {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  };
+};
