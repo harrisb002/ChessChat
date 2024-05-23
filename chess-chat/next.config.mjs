c443ec48de3d3acde6beffc8ed9f1eaa605ea7a4
harrisb002 <harrisbe@sonoma.edu>
2024-05-23 15:35:38 -0700

    creating chat query hook

--- chess-chat/next.config.mjs
+++ chess-chat/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
+  images: {
+    domains: ["utfs.io"],
+  },
 };
 
 export default nextConfig;
