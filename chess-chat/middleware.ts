d08f739b3402759f56c320adf8c351b1be841e10
harrisb002 <harrisbe002@gmail.com>
2025-02-27 00:38:03 -0800

    Ignore clerk for auth in socket

--- chess-chat/middleware.ts
+++ chess-chat/middleware.ts
@@ -1,13 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"] // Secure the route for uploading files
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ["/api/uploadthing"], // Secure the route for uploading files
+  ignoredRoutes: ["/api/socket/io"], // Ignore authentication for the socket route
 });
- 
+
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
